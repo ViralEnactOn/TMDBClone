b47b401458efebaacbf9c1f4ad41903d0118b27f
ViralEnactOn <vsjariwala@enacton.com>
2023-08-10 17:58:56 +0530

    Feat : Login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -17,6 +17,7 @@ function login() {
   const navigate = useNavigate();
 
   const handleSubmit = () => {
+    console.log(username, password);
     if (username.length === 0) {
       setUsernameError("Username cannot empty.");
       return;
@@ -70,9 +71,11 @@ function login() {
     handleCheckUser();
   }, []);
 
+  console.log(password);
+
   return (
     <>
-      <main className="min-w-max flex justify-center font-poppins bg-blue-200">
+      {/* <main className="min-w-max flex justify-center font-poppins bg-blue-200">
         <div className="container">
           <div className="flex justify-center min-h-screen">
             <div className="flex self-center w-96 h-96 bg-gray-200 justify-center rounded-lg">
@@ -118,7 +121,82 @@ function login() {
             </div>
           </div>
         </div>
-      </main>
+      </main> */}
+
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <form > */}
+          <div className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="text"
+                  autoComplete="email"
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {usernameError && (
+                  <div className="text-red-700 block text-sm font-medium leading-6">
+                    {usernameError}
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {passwordError && (
+                  <div className="text-red-700 block text-sm font-medium leading-6">
+                    {passwordError}
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div onClick={() => handleSubmit()}>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </div>
+          {/* </form> */}
+        </div>
+      </div>
     </>
   );
 }
